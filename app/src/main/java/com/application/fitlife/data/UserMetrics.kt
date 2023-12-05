c9af56004e9fe150b89c733546485aa143086de3
shrey333 <shreybahdiyadara33@gmail.com>
2023-12-05 01:09:19 -0700

    Fix: remove dependency on external UserMetrics

---
+++ app/src/main/java/com/application/fitlife/data/UserMetrics.kt
@@ -0,0 +1,10 @@
+package com.application.fitlife.data
+
+data class UserMetrics (
+    val id: Long,
+    val height: Double,
+    val weight: Double,
+    val score: Double,
+    val heartRate: Double,
+    val respiratoryRate: Double,
+)
